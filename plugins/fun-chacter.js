08f996e63655ca8665cbd54188a683a163211f72
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-03-15 19:12:20 +0000

    Update fun-chacter.js

--- plugins/fun-chacter.js
+++ plugins/fun-chacter.js
@@ -30,6 +30,6 @@ let handler = async (m, { conn, command, text, usedPrefix, participants }) => {
 }
 handler.help = ["character @tag"]
 handler.tags = ['fun']
-handler.command = /^(character)/i
+handler.command = /^character/i
 
 export default handler 
